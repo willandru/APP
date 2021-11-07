56164dada7b0d114f14751be2dd0249f2d159431
willandru <willandru@hotmail.com>
2021-11-07 01:46:53 -0400

    RAW BODY OF THE ENGINE

---
+++ APP1/src/main/UI.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.awt.Color;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingWorker;
+import scenes.MENU;
+import scenes.PLAYING;
+
+/**
+ *
+ * @author kaliw
+ */
+public class UI extends JFrame {
+    
+    private JPanel myPanel;
+    private static final int ANCHO=400;
+    private static final int ALTO=680;
+    
+    public boolean continua=true;
+    public boolean menuPintado=false;
+    public boolean settsPintado=false;
+    public boolean playPintado=false;
+    
+    public  PLAYING p;
+    public MENU m;
+    public Integer value=0;
+    
+    private Thread T1=null;
+    UI(){
+//        T1= new Thread(this);
+        setSize(ANCHO, ALTO);
+        setBackground(new Color(0x123456));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        myPanel=new JPanel ();
+        initMainPanel();
+        add(myPanel);
+        
+        // T1.start();
+        start();
+        setVisible(true);
+    }
+
+    private void initMainPanel() {
+        
+        myPanel.setSize(ANCHO, ALTO);
+        myPanel.setBackground(new Color(0x987297));
+        myPanel.setLayout(null);
+    }
+    
+   
+    private void start(){
+        SwingWorker<Void, Integer> worker = new SwingWorker<Void, Integer>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                
+                System.out.println("BEGIN WORKER");
+                
+               
+                
+              while(continua){ 
+                  Thread.sleep(20);
+                switch(StatesApp.gameState){
+              
+            case MENU:
+              
+                if(!menuPintado && value==0){
+                     m = new MENU();
+                m.setSize(ANCHO, ALTO);
+                //m.setBackground(Color.blue);
+                
+                myPanel.removeAll();
+                myPanel.add(m);
+                myPanel.revalidate();
+                myPanel.repaint();
+                 
+                System.out.println("Menu painted");
+                menuPintado=true;
+                }
+                System.out.println("Menu NUMBER: "+ m.getNumero());
+                
+                    publish(m.getNumero());
+                     System.out.println("MENU PUBLISHED");
+               
+               
+        
+                break;
+            case PLAYIN:
+                
+                if(!playPintado && value==1){
+                    p= new PLAYING();
+                p.setSize(ANCHO, ALTO);               
+                myPanel.removeAll();
+                myPanel.add(p);
+                myPanel.revalidate();
+                myPanel.repaint();
+                System.out.println("PLAYIN painted");
+                playPintado=true;
+                
+                }
+                 //publish(StatesApp.gameState);
+                System.out.println("PLAYING");
+                System.out.println("GO TO MENU NUMBER: "+ p.getNumber());
+                publish(p.getNumber());
+                break;
+            case SETTINGS:
+                
+                if(!settsPintado && value==2){
+                JPanel o = new JPanel();
+                o.setSize(ANCHO, ALTO);
+                o.setBackground(Color.red);
+                
+                myPanel.removeAll();
+                myPanel.add(o);
+                myPanel.revalidate();
+                myPanel.repaint();
+                System.out.println("SETTS painted");
+                settsPintado=true;
+                
+                }
+               // publish(StatesApp.gameState);
+                System.out.println("SETTINGS");
+                
+                break;
+            
+                    
+            
+                }
+               
+              }
+                
+
+                return null;
+            }
+
+            @Override
+            protected void process(List<Integer> chunks) {
+                  value= chunks.get(chunks.size()-1);
+                
+                 if(value==1){
+                     menuPintado=false;
+                     settsPintado=false;
+                     
+                     StatesApp.gameState= StatesApp.PLAYIN;
+                     
+                 }else if(value == 2){
+                     menuPintado=false;
+                     playPintado=false;
+                     StatesApp.gameState= StatesApp.SETTINGS;
+                 }else if(value == 0){
+                     settsPintado=false;
+                     playPintado=false;
+                     StatesApp.gameState= StatesApp.MENU;
+                 }
+                
+            }
+            
+           
+        };
+        worker.execute();
+    }
+
+    
+}
+
---
+++ APP1/src/scenes/MENU.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scenes;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import scenes.StatesApp;
+import static scenes.StatesApp.*;
+
+/**
+ *
+ * @author kaliw
+ */
+public class MENU extends JPanel implements ActionListener{
+    
+  
+
+     public JButton btnPlay, btnSetts, btnQuit;
+     public int numero=0;
+  
+    
+    public MENU(){
+       
+       
+        setLayout(null);
+        setBackground(new Color(0x123456));
+        initButtons();
+        addButtons();
+        setVisible(true);
+        
+    }
+
+     
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+         Object origen = ae.getSource();
+         if(origen == this.btnPlay){
+             numero=1;
+           StatesApp.gameState=PLAYIN;
+           System.out.println("actiion PLAYING");
+            
+        } else if(origen == this.btnSetts){
+            numero=2;
+            StatesApp.gameState=SETTINGS;
+            System.out.println("actiion SETTINGS");
+            System.out.println("state: "+ StatesApp.gameState);
+
+            
+        }else if(origen == this.btnQuit){
+            System.exit(0);
+        }
+        
+    }
+    
+
+    public int  getNumero(){
+        return numero;
+    }
+    
+
+
+    private void initButtons() {
+
+        btnPlay = new JButton("PLAY");
+        btnPlay.setBounds(130,300,124,40);
+       
+        btnSetts=new JButton("SETTINGS");
+        btnSetts.setBounds(130,390,124,40);
+        
+        btnQuit=new JButton("QUIT");
+        btnQuit.setBounds(130,480,124,40);
+          btnPlay.addActionListener(this);
+         btnSetts.addActionListener(this);
+          btnQuit.addActionListener(this);
+    }
+
+    private void addButtons() {
+        this.add(btnPlay);
+        this.add(btnSetts);
+        this.add(btnQuit);
+
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+}
